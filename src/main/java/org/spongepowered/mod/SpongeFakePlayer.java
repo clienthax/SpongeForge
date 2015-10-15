sconover/Sponge

Use Forge's FakePlayer as the simulated player

src/main/java/org/spongepowered/mod/SpongeFakePlayer.java
@@ -1,0 +1,44 @@
+/*
+ * This file is part of Sponge, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) SpongePowered <https://www.spongepowered.org>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.spongepowered.mod;
+
+import net.minecraft.util.Vec3;
+import net.minecraft.world.WorldServer;
+import net.minecraftforge.common.util.FakePlayer;
+import org.spongepowered.common.util.SimulatedPlayer;
+
+public class SpongeFakePlayer extends FakePlayer {
+
+    public SpongeFakePlayer(WorldServer world) {
+        super(world, SimulatedPlayer.profile);
+        this.playerNetServerHandler = new SimulatedPlayer.FakeNetHandler(this);
+    }
+
+    @Override
+    public Vec3 getPositionVector() {
+        return new Vec3(this.posX, this.posY, this.posZ);
+    }
+
+}

src/main/java/org/spongepowered/mod/SpongeMod.java
@@ -30,7 +30,9 @@ import com.google.common.eventbus.EventBus;
 import com.google.common.eventbus.Subscribe;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.WorldServer;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.DummyModContainer;
 import net.minecraftforge.fml.common.FMLCommonHandler;
@@ -70,6 +72,7 @@ import org.spongepowered.common.service.permission.SpongePermissionService;
 import org.spongepowered.common.service.persistence.SpongeSerializationService;
 import org.spongepowered.common.service.scheduler.SpongeScheduler;
 import org.spongepowered.common.service.sql.SqlServiceImpl;
+import org.spongepowered.common.util.PlayerSimulatorFactory;
 import org.spongepowered.common.util.SpongeHooks;
 import org.spongepowered.mod.event.SpongeEventHooks;
 import org.spongepowered.mod.guice.SpongeGuiceModule;
@@ -167,6 +170,14 @@ public class SpongeMod extends DummyModContainer implements PluginContainer {
         } catch (Throwable t) {
             this.controller.errorOccurred(this, t);
         }
+
+        PlayerSimulatorFactory.instance = new PlayerSimulatorFactory() {
+
+            @Override
+            protected EntityPlayerMP createPlayer(WorldServer world) {
+                return new SpongeFakePlayer(world);
+            }
+        };
     }
 
     @SubscribeEvent

clienthax/SpongeForge

src/main/java/org/spongepowered/mod/SpongeFakePlayer.java
@@ -1,0 +1,44 @@
+/*
+ * This file is part of Sponge, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) SpongePowered <https://www.spongepowered.org>
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.spongepowered.mod;
+
+import net.minecraft.util.Vec3;
+import net.minecraft.world.WorldServer;
+import net.minecraftforge.common.util.FakePlayer;
+import org.spongepowered.common.world.SimulatedPlayer;
+
+public class SpongeFakePlayer extends FakePlayer {
+
+    public SpongeFakePlayer(WorldServer world) {
+        super(world, SimulatedPlayer.profile);
+        this.playerNetServerHandler = new SimulatedPlayer.FakeNetHandler(this);
+    }
+
+    @Override
+    public Vec3 getPositionVector() {
+        return new Vec3(this.posX, this.posY, this.posZ);
+    }
+
+}

src/main/java/org/spongepowered/mod/SpongeMod.java
@@ -29,7 +29,9 @@ import com.google.common.eventbus.EventBus;
 import com.google.common.eventbus.Subscribe;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.WorldServer;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.DummyModContainer;
 import net.minecraftforge.fml.common.FMLCommonHandler;
@@ -73,6 +75,7 @@ import org.spongepowered.common.service.persistence.SpongeSerializationService;
 import org.spongepowered.common.service.scheduler.SpongeScheduler;
 import org.spongepowered.common.service.sql.SqlServiceImpl;
 import org.spongepowered.common.util.SpongeHooks;
+import org.spongepowered.common.world.PlayerSimulatorFactory;
 import org.spongepowered.mod.event.SpongeEventHooks;
 import org.spongepowered.mod.guice.SpongeGuiceModule;
 import org.spongepowered.mod.plugin.SpongeModPluginContainer;
@@ -179,6 +182,14 @@ public class SpongeMod extends DummyModContainer implements PluginContainer {
         } catch (Throwable t) {
             this.controller.errorOccurred(this, t);
         }
+
+        PlayerSimulatorFactory.instance = new PlayerSimulatorFactory() {
+
+            @Override
+            protected EntityPlayerMP createPlayer(WorldServer world) {
+                return new SpongeFakePlayer(world);
+            }
+        };
     }
 
     @SubscribeEvent